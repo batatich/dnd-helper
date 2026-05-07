522c1282af6c79b7743db079be729dd4bafd9a2a
batatich <timuraa008@gmail.com>
2026-05-07 19:56:49 +0300

    refactor: add reusable Card and Badge components

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,14 @@
+type Props = React.HTMLAttributes<HTMLDivElement> & {
+  children: React.ReactNode
+}
+
+export function Card({ children, className = '', ...props }: Props) {
+  return (
+    <div
+      {...props}
+      className={`bg-gray-800/90 border border-gray-700 rounded-xl p-4 shadow-sm ${className}`}
+    >
+      {children}
+    </div>
+  )
+}
